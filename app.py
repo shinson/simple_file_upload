b016af4bd90575754ddb16ec44e7b91c3769702d
soniahinson <sonia@adhocteam.us>
2019-05-26 23:56:58 -0400

    Add database * Add packages for db integration and migration management * Add models for customers, users, and purchases

--- app.py
+++ app.py
@@ -1,15 +1,54 @@
 import os
 from flask import Flask, flash, request, redirect, url_for, render_template
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from werkzeug.utils import secure_filename
 
+
+# CONFIG
 UPLOAD_FOLDER = '/tmp'
+ALLOWED_EXTENSIONS = set(['tsv'])
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost:5432/purchase_log'
 
-dir_path = os.path.dirname(os.path.realpath(__file__))
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 
+# MODELS
+class Customer(db.Model):
+    __tablename__ = 'customers'
+
+    id = db.Column(db.Integer, primary_key=True)
+    fist_name = db.Column(db.Text, nullable=False)
+    last_name = db.Column(db.Text, nullable=False)
+    street_address = db.Column(db.Text, nullable=False)
+    state = db.Column(db.String(2), nullable=False)
+    zipcode = db.Column(db.String(5), nullable=False)
+
+
+class Product(db.Model):
+    __tablename__ = 'products'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+
+
+class OrderStatus(db.Model):
+    __tablename__ = 'order_statuses'
+
+    id = db.Column(db.Integer, primary_key=True)
+    customer_id = db.Column(db.Integer, db.ForeignKey('customers.id'), nullable=False)
+    product_id = db.Column(db.Integer, db.ForeignKey('products.id'), nullable=False)
+    price = db.Column(db.Text, nullable=False)
+    status = db.Column(db.Enum("new", "cancel", name="order_enum", create_type=False))
+    created_at = db.Column(db.DateTime(timezone=True), nullable=False)
+    updated_at = db.Column(db.DateTime(timezone=True), nullable=False)
+
+
+# VIEWS
 @app.route('/')
 def upload():
     return render_template("upload.html")
@@ -25,7 +64,7 @@ def upload_file():
         file = request.files['file']
 
         if file.filename == '':
-            flash('No selected file')
+            flash('Invalid file!')
             return redirect(request.url)
         if file:
             filename = secure_filename(file.filename)
---
+++ migrations/versions/dbe3795f3a73_initial_database.py
@@ -0,0 +1,51 @@
+"""Initial Database
+
+Revision ID: dbe3795f3a73
+Revises: 
+Create Date: 2019-05-26 23:48:06.521619
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'dbe3795f3a73'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.create_table('customers',
+    sa.Column('id', sa.Integer(), nullable=False),
+    sa.Column('fist_name', sa.Text(), nullable=False),
+    sa.Column('last_name', sa.Text(), nullable=False),
+    sa.Column('street_address', sa.Text(), nullable=False),
+    sa.Column('state', sa.String(length=2), nullable=False),
+    sa.Column('zipcode', sa.String(length=5), nullable=False),
+    sa.PrimaryKeyConstraint('id')
+    )
+    op.create_table('products',
+    sa.Column('id', sa.Integer(), nullable=False),
+    sa.Column('name', sa.String(length=100), nullable=False),
+    sa.PrimaryKeyConstraint('id')
+    )
+    op.create_table('order_statuses',
+    sa.Column('id', sa.Integer(), nullable=False),
+    sa.Column('customer_id', sa.Integer(), nullable=False),
+    sa.Column('product_id', sa.Integer(), nullable=False),
+    sa.Column('price', sa.Text(), nullable=False),
+    sa.Column('status', sa.Enum('new', 'cancel', name='order_enum'), nullable=True),
+    sa.Column('created_at', sa.DateTime(timezone=True), nullable=False),
+    sa.Column('updated_at', sa.DateTime(timezone=True), nullable=False),
+    sa.ForeignKeyConstraint(['customer_id'], ['customers.id'], ),
+    sa.ForeignKeyConstraint(['product_id'], ['products.id'], ),
+    sa.PrimaryKeyConstraint('id')
+    )
+
+
+def downgrade():
+    op.drop_table('order_statuses')
+    op.drop_table('products')
+    op.drop_table('customers')
